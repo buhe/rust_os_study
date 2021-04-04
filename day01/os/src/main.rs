2098af3a3235690f59438d35c6a5cb78508e996e
buhe <hare.bu@herewhite.com>
2021-04-04 13:28:40 +0800

    add asm

--- day01/os/src/main.rs
+++ day01/os/src/main.rs
@@ -1,23 +1,56 @@
 #![no_std]
 #![no_main]
+#![feature(global_asm)]
 #![feature(llvm_asm)]
-// use core::fmt::{self, Write};
+use core::fmt::{self, Write};
 mod lang_items;
 mod sbi;
-
 use crate::sbi::sbi_call;
+global_asm!(include_str!("entry.asm"));
 
-const SBI_SHUTDOWN: usize = 93;
-
-pub fn shutdown() -> ! {
-    sbi_call(SBI_SHUTDOWN, 0, 0, 0);
-    panic!("It should shutdown!");
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|a| {
+        unsafe { (a as *mut u8).write_volatile(0) }
+    });
 }
 
-#[no_mangle]
-extern "C" fn _start() {
-    shutdown();
+const SBI_CONSOLE_PUTCHAR: usize = 1;
+
+pub fn console_putchar(c: usize) {
+    sbi_call(SBI_CONSOLE_PUTCHAR, c, 0, 0);
 }
+
+struct Stdout;
+impl Write for Stdout {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        //sys_write(STDOUT, s.as_bytes());
+        for c in s.chars() {
+            console_putchar(c as usize);
+        }
+        Ok(())
+    }
+}
+
+
+// mod sbi;
+
+
+
+// const SBI_SHUTDOWN: usize = 93;
+
+// pub fn shutdown() -> ! {
+//     sbi_call(SBI_SHUTDOWN, 0, 0, 0);
+//     panic!("It should shutdown!");
+// }
+
+// #[no_mangle]
+// extern "C" fn _start() {
+//     shutdown();
+// }
 // fn syscall(id: usize, args: [usize; 3]) -> isize {
 //     let mut ret: isize;
 //     unsafe {
@@ -50,24 +83,29 @@ extern "C" fn _start() {
 //     }
 // }
 
-// pub fn print(args: fmt::Arguments) {
-//     Stdout.write_fmt(args).unwrap();
-// }
+pub fn print(args: fmt::Arguments) {
+    Stdout.write_fmt(args).unwrap();
+}
 
-// #[macro_export]
-// macro_rules! print {
-//     ($fmt: literal $(, $($arg: tt)+)?) => {
-//         $crate::console::print(format_args!($fmt $(, $($arg)+)?));
-//     }
-// }
+#[macro_export]
+macro_rules! print {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        $crate::console::print(format_args!($fmt $(, $($arg)+)?));
+    }
+}
 
-// #[macro_export]
-// macro_rules! println {
-//     ($fmt: literal $(, $($arg: tt)+)?) => {
-//         print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
-//     }
-// }
-
+#[macro_export]
+macro_rules! println {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
+    }
+}
+#[no_mangle]
+pub fn rust_main() -> ! {
+    clear_bss();
+    println!("Hello, world! buhe");
+    loop{}
+}
 
 // // fn main() {
 //     // println!("Hello, world!");
