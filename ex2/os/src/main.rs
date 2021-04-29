08cf1b154cf5ae4d2565e3720a28e369a5f07311
buhe <hare.bu@herewhite.com>
2021-04-29 17:51:02 +0800

    ex2 part3

--- ex2/os/src/main.rs
+++ ex2/os/src/main.rs
@@ -44,7 +44,18 @@ extern crate alloc;
 pub extern "C" fn rust_main() {
     interrupt::init();
     memory::init();
-    println!("{}", *memory::config::KERNEL_END_ADDRESS);
+    // 物理页分配
+    for _ in 0..2 {
+        let frame_0 = match memory::frame::FRAME_ALLOCATOR.lock().alloc() {
+            Result::Ok(frame_tracker) => frame_tracker,
+            Result::Err(err) => panic!("{}", err)
+        };
+        let frame_1 = match memory::frame::FRAME_ALLOCATOR.lock().alloc() {
+            Result::Ok(frame_tracker) => frame_tracker,
+            Result::Err(err) => panic!("{}", err)
+        };
+        println!("{} and {}", frame_0.address(), frame_1.address());
+    }
 
     panic!()
     // panic!("end of rust_main")
