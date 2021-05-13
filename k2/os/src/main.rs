7a77755cf398ca9d769a7fe08b1fb76d6d0e7468
buhe <hare.bu@herewhite.com>
2021-05-13 13:23:15 +0800

    k2

--- k2/os/src/main.rs
+++ k2/os/src/main.rs
@@ -8,15 +8,21 @@
 mod console;
 mod lang_items;
 mod sbi;
+mod syscall;
+mod trap;
+mod batch;
 
 global_asm!(include_str!("entry.asm"));
+global_asm!(include_str!("link_app.S"));
 
 fn clear_bss() {
     extern "C" {
         fn sbss();
         fn ebss();
     }
-    (sbss as usize..ebss as usize).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) });
+    (sbss as usize..ebss as usize).for_each(|a| {
+        unsafe { (a as *mut u8).write_volatile(0) }
+    });
 }
 
 #[no_mangle]
@@ -34,6 +40,7 @@ pub fn rust_main() -> ! {
         fn boot_stack_top();
     }
     clear_bss();
+    
     println!("Hello, world!");
     info!(".text [{:#x}, {:#x})", stext as usize, etext as usize);
     error!(".rodata [{:#x}, {:#x})", srodata as usize, erodata as usize);
@@ -43,5 +50,8 @@ pub fn rust_main() -> ! {
         boot_stack as usize, boot_stack_top as usize
     );
     warn!(".bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
-    panic!("Shutdown machine!");
+
+    trap::init();
+    batch::init();
+    batch::run_next_app();
 }
--- k2/user/src/bin/00hello_world.rs
+++ k2/user/src/bin/00hello_world.rs
@@ -8,8 +8,8 @@ extern crate user_lib;
 #[no_mangle]
 fn main() -> i32 {
     println!("Hello, world!buhe app");
-    unsafe {
-        llvm_asm!("sret");
-    }
+    // unsafe {
+    //     llvm_asm!("sret");
+    // }
     0
 }
