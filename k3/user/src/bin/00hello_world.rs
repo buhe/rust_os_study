3749a24a9097d8eeba573ac470d675230277db3a
buhe <hare.bu@herewhite.com>
2021-05-14 23:09:54 +0800

    Update 00hello_world.rs

--- k3/user/src/bin/00hello_world.rs
+++ k3/user/src/bin/00hello_world.rs
@@ -5,15 +5,11 @@
 #[macro_use]
 extern crate user_lib;
 
-use user_lib::{get_time, yield_};
-
 #[no_mangle]
 fn main() -> i32 {
-    let current_timer = get_time();
-    let wait_for = current_timer + 3000;
-    while get_time() < wait_for {
-        yield_();
+    println!("Hello, world!buhe app");
+    unsafe {
+        llvm_asm!("sret");
     }
-    println!("Test sleep OK!");
     0
 }
