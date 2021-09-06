d702340e9e0d7182b0ff0256e48b230afedf5cfe
buhe <hare.bu@herewhite.com>
2021-09-06 19:58:13 +0800

    print os

--- bugu/os/src/lang_items.rs
+++ bugu/os/src/lang_items.rs
@@ -1,7 +1,17 @@
-// use crate::sbi::shutdown;
+use crate::sbi::shutdown;
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    panic!("It should shutdown!");
+fn panic(info: &PanicInfo) -> ! {
+    if let Some(location) = info.location() {
+        error!(
+            "Panicked at {}:{} {}",
+            location.file(),
+            location.line(),
+            info.message().unwrap()
+        );
+    } else {
+        error!("Panicked: {}", info.message().unwrap());
+    }
+    shutdown()
 }
