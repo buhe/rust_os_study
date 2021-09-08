f9cd987bed9cc1e0d1a8c0574752782363619fb0
buhe <hare.bu@herewhite.com>
2021-09-08 22:06:28 +0800

    build pass

--- bugu/os/src/main.rs
+++ bugu/os/src/main.rs
@@ -8,15 +8,21 @@
 mod console;
 mod lang_items;
 mod sbi;
+mod syscall;
+mod trap;
+mod batch;
 
 global_asm!(include_str!("entry.asm"));
+global_asm!(include_str!("link_app.S"));
 
 fn clear_bss() {
     extern "C" {
         fn sbss();
         fn ebss();
     }
-    (sbss as usize..ebss as usize).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) });
+    (sbss as usize..ebss as usize).for_each(|a| {
+        unsafe { (a as *mut u8).write_volatile(0) }
+    });
 }
 
 #[no_mangle]
@@ -32,10 +38,10 @@ pub fn rust_main() -> ! {
         fn ebss();
         fn boot_stack();
         fn boot_stack_top();
-        fn bugu();
     }
     clear_bss();
-    println!("Hello, world!223");
+    
+    println!("Hello, world!");
     info!(".text [{:#x}, {:#x})", stext as usize, etext as usize);
     error!(".rodata [{:#x}, {:#x})", srodata as usize, erodata as usize);
     debug!(".data [{:#x}, {:#x})", sdata as usize, edata as usize);
@@ -44,6 +50,8 @@ pub fn rust_main() -> ! {
         boot_stack as usize, boot_stack_top as usize
     );
     warn!(".bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
-    info!("bugu is here {:#x}", bugu as usize);
-    panic!("Shutdown machine!");
+
+    trap::init();
+    batch::init();
+    batch::run_next_app();
 }
