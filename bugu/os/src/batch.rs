711abca6051fa2dc56925411d8de32a73724a052
buhe <hare.bu@herewhite.com>
2021-09-09 10:50:12 +0800

    Update batch.rs

--- bugu/os/src/batch.rs
+++ bugu/os/src/batch.rs
@@ -80,9 +80,9 @@ impl AppManagerInner {
 
     pub fn get_current_app(&self) -> usize { self.current_app }
     // curreny_app 会比 app_id 大 1
-    pub fn get_current_app_mem_range(&self) -> (usize, usize) {
-        (self.app_start[self.current_app - 1], self.app_start[self.current_app])
-    }
+    // pub fn get_current_app_mem_range(&self) -> (usize, usize) {
+    //     (self.app_start[self.current_app - 1], self.app_start[self.current_app])
+    // }
     pub fn move_to_next_app(&mut self) {
         self.current_app += 1;
     }
@@ -117,9 +117,9 @@ pub fn print_app_info() {
 }
 
 
-pub fn get_current_app_mem_range() -> (usize, usize) {
-    APP_MANAGER.inner.borrow().get_current_app_mem_range()
-}
+// pub fn get_current_app_mem_range() -> (usize, usize) {
+//     APP_MANAGER.inner.borrow().get_current_app_mem_range()
+// }
 
 pub fn run_next_app() -> ! {
     let current_app = APP_MANAGER.inner.borrow().get_current_app();
